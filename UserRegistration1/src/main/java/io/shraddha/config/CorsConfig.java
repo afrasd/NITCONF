708838b717f81cbc6e529461ebae28716eae34c6
“Siri <siripagadala2003@gmail.com>
2024-02-04 23:35:22 +0530

    changed only reviewed page's endpoint to return a json response insteal of a html view

---
+++ UserRegistration1/src/main/java/io/shraddha/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package io.shraddha.config;
+
+//public class CorsConfig {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3001")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
