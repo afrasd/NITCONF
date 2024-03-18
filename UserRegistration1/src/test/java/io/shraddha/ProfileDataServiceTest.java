0227212a11ebfccc3f963657d2fbc285122241d3
shraddhayelamarthy <109479657+shraddhayelamarthy@users.noreply.github.com>
2024-03-19 00:13:32 +0530

    Update ProfileDataServiceTest.java

--- UserRegistration1/src/test/java/io/shraddha/ProfileDataServiceTest.java
+++ UserRegistration1/src/test/java/io/shraddha/ProfileDataServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import static org.junit.Assert.*;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
